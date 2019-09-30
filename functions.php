5554c91b7e9f2763fd3473e82d97c5221eab0cad
Noah <48299180+sovsay11@users.noreply.github.com>
2019-09-30 12:30:31 -0600

    User Redirect
    In the functions.php file, got the redirect for admin and clients to work

--- functions.php
+++ functions.php
@@ -292,6 +292,11 @@ function twentynineteen_colors_css_wrap() {
 }
 add_action( 'wp_head', 'twentynineteen_colors_css_wrap' );
 
+function user_login_redirect( $redirect_to, $request, $user  ) {
+	return ( is_array( $user->roles ) && in_array( 'administrator', $user->roles ) ) ? site_url("admin-main-page") : site_url();
+}
+add_filter( 'login_redirect', 'user_login_redirect', 10, 3 );
+
 /**
  * SVG Icons class.
  */
